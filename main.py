35e3c45989bccc520a382297a79abbf09181f2c2
Emmanuell007 <137746754+Emmanuell007@users.noreply.github.com>
2024-04-07 16:47:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,29 @@
 from flask import Flask, request, jsonify
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+import requests
+from bs4 import BeautifulSoup
 import re
 
 app = Flask(__name__)
 
 def encontrar_cantidad(cadena):
-    patron = r"(\d+)\s*(Tableta|Cápsula|Pastilla)s?"
+    patron = r"(\d+)\s*(Tableta|Cápsula|Pastilla|ml)s?"
     coincidencias = re.search(patron, cadena, re.IGNORECASE)
+    
     if coincidencias:
         cantidad = int(coincidencias.group(1))
         palabra = coincidencias.group(2)
+        
         return cantidad, palabra
     else:
         return 0, None
 
-@app.route('/')
-def root():
-    return "huevos al furro"
-
-
 @app.route('/scrape', methods=['POST'])
 def scrape_productos():
-    search_term = request.json.get('medicamentos', '')
-    
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--log-level=3") 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-    
-    medicamentos_lista = search_term.split("\n")
-    
     medicamento = {
         "Medicamentos": []
     }
+    search_term = request.json.get('medicamentos', '')
+    medicamentos_lista = search_term.split("\n")
 
     for product in medicamentos_lista:
         nombre = product.split(" ")
@@ -49,53 +32,46 @@ def scrape_productos():
             "Presentaciones": []
         }
         
-        url = f"https://www.farmaciasanpablo.com.mx/search/?query={product}"
-        driver.get(url)
-        
-        try:
-            wait = WebDriverWait(driver, 10)
-            wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".nameProduct")))
-        except TimeoutException:
-            pass
-        
-        results = driver.find_elements(By.CSS_SELECTOR, ".nameProduct")
+        url = f"https://farmaciasgi.com.mx/?s={product}&post_type=product&dgwt_wcas=1"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #woocommerce-Price-amount
+        results = soup.select("h2.woocommerce-loop-product__title")
         num_results = len(results)
         
         if num_results != 0:
             num_products = min(num_results, 3)
             
             for i in range(num_products):
-                product_name = results[i].text
-                product_desc = driver.find_elements(By.CSS_SELECTOR, ".custom-postop")[i].text
-                product_cost = driver.find_elements(By.CSS_SELECTOR, ".price p")[i].text
+                product_name = results[i].get_text(strip=True)
+                product_price_elements = soup.select("span.woocommerce-Price-amount")
+                precio_num = product_price_elements[i].text
 
-                precio_num = "".join(c for c in product_cost if c.isdigit() or c == '.')
-                precio_num = float(precio_num)
-
-                cant1, tip1 = encontrar_cantidad(product_desc)
+                product_cost = "".join(c for c in precio_num if c.isdigit() or c == '.')
+                product_cost = float(product_cost)
                 cant2, tip2 = encontrar_cantidad(product_name)
 
-                if cant1 != 0:
-                    cantidad = f"{cant1} {tip1}s"
-                    medicamento_actual["Presentaciones"].append({
-                        "Precio": precio_num,
-                        "Farmacia": "San Pablo",
-                        "Cantidad": cantidad
-                    })
-                elif cant2 != 0:
-                    cantidad = f"{cant2} {tip2}s"
-                    medicamento_actual["Presentaciones"].append({
-                        "Precio": precio_num,
-                        "Farmacia": "San Pablo",
-                        "Cantidad": cantidad
-                    })
+                if cant2 != 0:
+                    if tip2 !="ml":
+                        cantidad = f"{cant2} {tip2}s"
+                        medicamento_actual["Presentaciones"].append({
+                            "Precio": precio_num,
+                            "Farmacia": "Gi",
+                            "Cantidad": cantidad
+                        })
+                    else:
+                        cantidad = f"{cant2} {tip2}"
+                        medicamento_actual["Presentaciones"].append({
+                            "Precio": precio_num,
+                            "Farmacia": "Gi",
+                            "Cantidad": cantidad
+                        })
                 else:
                     medicamento_actual["Presentaciones"].append({
                         "Precio": precio_num,
-                        "Farmacia": "San Pablo",
+                        "Farmacia": "Gi",
                         "Cantidad": "No disponible"
                     })
-        
         else:
             medicamento_actual["Presentaciones"].append({
                 "Precio": 0,
@@ -105,9 +81,9 @@ def scrape_productos():
         
         medicamento["Medicamentos"].append(medicamento_actual)
 
-    driver.quit()
-    
     return jsonify(medicamento)
-
+    
 if __name__ == '__main__':
     app.run()
+
+
